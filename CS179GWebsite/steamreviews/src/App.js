900df5afc52cf568833e34c5fe0a3be4fae1e74f
Maker424 <jimmydelgado0812@gmail.com>
2024-05-29 17:24:47 -0700

    Added component files to contain work for each separate task

--- CS179GWebsite/steamreviews/src/App.js
+++ CS179GWebsite/steamreviews/src/App.js
@@ -3,21 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Website">
+      <div className="Web-Header">
+        <h1>STEAMY REVIEWS</h1>
+      </div>
+      <div className="Web-Body"> 
+      
+      </div>
     </div>
   );
 }
